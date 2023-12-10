b622ed2ddbbd03d6b3bb7b4333183927164c24ce
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-12-10 10:37:52 +0100

    black formatting

--- pkg_testing_tools/use.py
+++ pkg_testing_tools/use.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import portage
 import random
+
+import portage
 
 
 def iuse_match_always_true(flag):
@@ -12,7 +13,7 @@ def strip_use_flags(flags):
     stripped_flags = []
 
     for flag in flags:
-        if flag[0] in ['+', '-']:
+        if flag[0] in ["+", "-"]:
             flag = flag[1:]
 
         stripped_flags.append(flag)
@@ -24,27 +25,20 @@ def filter_out_use_flags(flags):
     new_flags = []
 
     ignore_flags_with_prefix = (
-            'elibc_',
-            'eglibc_',
-            'video_cards_',
-            'linguas_',
-            'l10n_',
-            'kernel_',
-            'abi_',
-            'python_target_',
-            'python_targets_',
-            'ruby_targets_',
-            'cpu_flags_'
+        "elibc_",
+        "eglibc_",
+        "video_cards_",
+        "linguas_",
+        "l10n_",
+        "kernel_",
+        "abi_",
+        "python_target_",
+        "python_targets_",
+        "ruby_targets_",
+        "cpu_flags_",
     )
 
-    ignore_flags = set([
-            'debug',
-            'doc',
-            'test',
-            'selinux',
-            'split-usr',
-            'pic'
-    ])
+    ignore_flags = set(["debug", "doc", "test", "selinux", "split-usr", "pic"])
 
     # some flags that *most* likely we shouldn't shuffle and test.
     for flag in flags:
@@ -55,11 +49,13 @@ def filter_out_use_flags(flags):
 
 
 def atom_to_cpv(atom):
-    return portage.db[portage.root]['porttree'].dbapi.match(atom)[0]
+    return portage.db[portage.root]["porttree"].dbapi.match(atom)[0]
 
 
 def get_package_flags(cpv):
-    flags = portage.db[portage.root]['porttree'].dbapi.aux_get(cpv, ['IUSE', 'REQUIRED_USE'])
+    flags = portage.db[portage.root]["porttree"].dbapi.aux_get(
+        cpv, ["IUSE", "REQUIRED_USE"]
+    )
 
     use_flags = strip_use_flags(flags[0].split())
     use_flags = filter_out_use_flags(use_flags)
@@ -67,17 +63,14 @@ def get_package_flags(cpv):
 
     ruse_flags = flags[1].split()
 
-    return [
-        use_flags,
-        ruse_flags
-    ]
+    return [use_flags, ruse_flags]
 
 
 def get_use_flags_toggles(index, iuse):
     on_off_switches = []
 
     for i in range(len(iuse)):
-        if ((2**i) & index):
+        if (2**i) & index:
             on_off_switches.append("")
         else:
             on_off_switches.append("-")
@@ -88,7 +81,7 @@ def get_use_flags_toggles(index, iuse):
 
 
 def get_use_combinations(iuse, ruse, max_use_combinations):
-    all_combinations_count = 2**len(iuse)
+    all_combinations_count = 2 ** len(iuse)
 
     valid_use_flags_combinations = []
 
@@ -96,8 +89,11 @@ def get_use_combinations(iuse, ruse, max_use_combinations):
         random.seed()
         checked_combinations = set()
 
-        while len(valid_use_flags_combinations) < max_use_combinations and len(checked_combinations) < all_combinations_count:
-            index = random.randint(0, all_combinations_count-1)
+        while (
+            len(valid_use_flags_combinations) < max_use_combinations
+            and len(checked_combinations) < all_combinations_count
+        ):
+            index = random.randint(0, all_combinations_count - 1)
 
             if index in checked_combinations:
                 continue
@@ -106,13 +102,17 @@ def get_use_combinations(iuse, ruse, max_use_combinations):
 
             flags = get_use_flags_toggles(index, iuse)
 
-            if portage.dep.check_required_use(" ".join(ruse), flags, iuse_match_always_true):
+            if portage.dep.check_required_use(
+                " ".join(ruse), flags, iuse_match_always_true
+            ):
                 valid_use_flags_combinations.append(flags)
     else:
         for index in range(0, all_combinations_count):
             flags = get_use_flags_toggles(index, iuse)
 
-            if portage.dep.check_required_use(" ".join(ruse), flags, iuse_match_always_true):
+            if portage.dep.check_required_use(
+                " ".join(ruse), flags, iuse_match_always_true
+            ):
                 valid_use_flags_combinations.append(flags)
 
     return valid_use_flags_combinations
